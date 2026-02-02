dbbee3eee67f2587bfea9acbff4ceb21cf8513a9
y35358186-afk <y35358186@gmail.com>
2026-02-02 05:53:34 +0100

    Add service worker
    Pwa service worker

---
+++ Sw.js
@@ -0,0 +1,45 @@
+/**
+ * Service Worker for Pavani Bot Creator V10
+ * Enables PWA functionality and offline support
+ */
+
+const CACHE_NAME = 'pavani-v10-cache-v1';
+const urlsToCache = [
+    '/',
+    '/static/style.css',
+    '/static/app.js',
+    '/manifest.json'
+];
+
+// Install event
+self.addEventListener('install', (event) => {
+    event.waitUntil(
+        caches.open(CACHE_NAME)
+            .then((cache) => cache.addAll(urlsToCache))
+    );
+});
+
+// Activate event
+self.addEventListener('activate', (event) => {
+    event.waitUntil(
+        caches.keys().then((cacheNames) => {
+            return Promise.all(
+                cacheNames.map((cacheName) => {
+                    if (cacheName !== CACHE_NAME) {
+                        return caches.delete(cacheName);
+                    }
+                })
+            );
+        })
+    );
+});
+
+// Fetch event
+self.addEventListener('fetch', (event) => {
+    event.respondWith(
+        caches.match(event.request)
+            .then((response) => {
+                return response || fetch(event.request);
+            })
+    );
+});
